b7b463aac985a5901d08b6dadb48fbbf2d7f9f67
Ofir <ofirbnaim@gmail.com>
2021-09-16 14:06:11 +0300

    middle of section 3 - end of part 25

--- API/Startup.cs
+++ API/Startup.cs
@@ -29,13 +29,15 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //Makes the connection with the DB, using _config witch is the config file (appsettings.Development.json)
+            //Alow to use the data in DB every part in the programm by adding "_config" witch is the config file (appsettings.Development.json) of my DB
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
 
             services.AddControllers();
+            services.AddCors();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -56,12 +58,15 @@ namespace API
 
             app.UseRouting();
 
+            //Allow to the front-end to access to the API - the back-end
+            app.UseCors( policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
-        }
+        }    
     }
 }
