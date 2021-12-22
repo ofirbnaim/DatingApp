c7264138250c5304a96f174e5901776a3bf06113
Ofir <ofirbnaim@gmail.com>
2021-12-22 07:42:56 +0200

    Entity Framework relationship - End of lesson 87

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities
 {
+    //When Entity Framework creates this table - its going to call "Photos"
+    //I can see it in the 'ExtendedUserEntity' Migration that i created
+    [Table("Photos")]
     public class Photo
     {
         public int Id { get; set; }
         public string Url { get; set; }
         public bool IsMain { get; set; }
         public string PublicId { get; set; }
+
+        //Define the relationship between AppUser and Photo
+        public AppUser AppUser { get; set; }
+        public int AppUserId { get; set; }
     }
 }
